fd7d8fe14f8f52c312357c47c269a222c2433b9b
Guruprasad Raikar <gp.raikar@planview.com>
2024-07-18 09:20:02 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
+      <div><h1>Greetings!!</h1></div>
+      <div><h2>Anup's Team</h2></div>
       <Counter />
     </div>
   );
